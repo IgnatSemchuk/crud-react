37bef0725f38acff6c21a50d7fd66c40a2510864
Your Name <semchukat@gmail.com>
2019-02-27 13:11:28 +0200

    Updates

---
+++ precache-manifest.8874b461a8b4df68d7c824d977ea64c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8ae5259a6b519baa6e5",
+    "url": "/crud-react/static/js/runtime~main.a8ae5259.js"
+  },
+  {
+    "revision": "dddaffcb9447291ce7d9",
+    "url": "/crud-react/static/js/main.dddaffcb.chunk.js"
+  },
+  {
+    "revision": "ca823686fbd20bf3148b",
+    "url": "/crud-react/static/js/2.ca823686.chunk.js"
+  },
+  {
+    "revision": "dddaffcb9447291ce7d9",
+    "url": "/crud-react/static/css/main.02a87647.chunk.css"
+  },
+  {
+    "revision": "07cba00425f14f6b5c4265c5f9a41f62",
+    "url": "/crud-react/index.html"
+  }
+];
